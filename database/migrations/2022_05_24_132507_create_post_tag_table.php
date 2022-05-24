45328796724908d3cdcf01c8a6845ca73bcac943
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-05-24 15:25:40 +0200

    tag migration

--- database/migrations/2022_05_24_132507_create_post_tag_table.php
+++ database/migrations/2022_05_24_132507_create_post_tag_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTagsTable extends Migration
+class CreatePostTagTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreateTagsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tags', function (Blueprint $table) {
+        Schema::create('post_tag', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50);
-            $table->string('slug');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ class CreateTagsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tags');
+        Schema::dropIfExists('post_tag');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,7 @@ Route::get("{any?}", function() {
 })->where("any", ".*");
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
