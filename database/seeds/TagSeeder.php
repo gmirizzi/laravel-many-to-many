4187bfaa8b8c9481fbb2ad61d48eb2b951c72207
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-05-24 15:30:29 +0200

    tag seeder

--- database/seeds/TagSeeder.php
+++ database/seeds/TagSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Tag;
+use Illuminate\Support\Str;
 
 class TagSeeder extends Seeder
 {
@@ -11,6 +13,13 @@ class TagSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $tags = ['cani', 'gatti', 'Roma', 'Elon Musk', 'Bitcoin', 'Carbonara', 'funghi', 'php', 'oop', 'javascript', 'Italia'];
+
+        foreach ($tags as $tag) {
+            Tag::create([
+                'name'  => $tag,
+                'slug'  => Str::slug($tag)
+            ]);
+        }
     }
 }
